07cf6ecb0b9c4d765920d100e50fdd5faa3156e8
Sophie JACQUOT <sophienanterre@gmail.com>
2020-12-04 16:59:20 +0100

    creation of an Adapter to handle data between the API and the view

---
+++ app/src/main/java/com/example/pokedex2020/adapter/PokemonListAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.pokedex2020.adapter
+
+//Adapters provide a binding from an app-specific data set to views that are displayed within a RecyclerView
+
+class PokemonListAdapter {
+}
