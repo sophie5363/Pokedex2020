9d8edfb11527bdd6acd9119e54da06589bc129e6
Sophie JACQUOT <sophienanterre@gmail.com>
2020-12-04 15:52:22 +0100

    Creation of models to handle data (Pokedex, Pokemon, Evolution)

---
+++ app/src/main/java/com/example/pokedex2020/model/Evolution.kt
@@ -0,0 +1,8 @@
+package com.example.pokedex2020.model
+
+//Properties of pokemon's evolution
+//number and name
+class Evolution {
+    var num:String?=null
+    var name:String?=null
+}
---
+++ app/src/main/java/com/example/pokedex2020/model/Pokedex.kt
@@ -0,0 +1,11 @@
+package com.example.pokedex2020.model
+
+class Pokedex {
+    /*
+    public List<Pokemon> pokemon { get; set; }
+     */
+
+    //Creation of a Pokemon list
+    //initialized to null
+    var pokemon:List<Pokemon>? = null
+}
